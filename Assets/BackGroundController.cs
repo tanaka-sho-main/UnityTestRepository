e14bfe3dfe0aa4bb3bd713f7fc85df2f57e5cb2e
tanaka-sho-main <132259074+tanaka-sho-main@users.noreply.github.com>
2025-02-04 01:50:08 +0900

    Update BackGroundController.cs

--- Assets/BackGroundController.cs
+++ Assets/BackGroundController.cs
@@ -2,26 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackGroundController : MonoBehaviour
-{
+public class BackGroundController : MonoBehaviour {
 	// Start is called before the first frame update
-	void Start()
-	{
+	void Start() {
 		DontDestroyOnLoad(gameObject);
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
+	/// <summary>値を 0 ~ 1 の範囲に正規化する</summary>
+	/// <param name="value">値</param>
+	/// <returns>正規化された値</returns>
+	void Update() {
 	}
 
-	private void FixedUpdate()
-	{
+	private void FixedUpdate() {
 		transform.Translate(0, -0.03f, 0);
-		if(transform.position.y < -4.9f)
-		{
+		if (transform.position.y < -4.9f) {
 			transform.position = new Vector3(0, 4.9f, 0);
-			// ƒXƒ^[ƒgƒ{ƒ^ƒ“
 		}
 	}
 }
